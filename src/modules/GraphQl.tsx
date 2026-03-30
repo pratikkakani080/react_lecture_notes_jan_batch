5a4414fd8c7a0739bf32f3555880fbe3851c7b35
Pratik <pratik080.rejoice@gmail.com>
2026-03-30 15:57:10 +0530

    fix: build error

--- src/modules/GraphQl.tsx
+++ src/modules/GraphQl.tsx
@@ -1,20 +1,14 @@
 import { useQuery } from '@apollo/client/react'
-import React from 'react'
-import { GET_LOCATIONS, TEST_QUERY } from '../queries'
+import { TEST_QUERY } from '../queries'
 
 function GraphQl() {
-    const { data, loading, error } = useQuery(TEST_QUERY)
+    const { data, loading } = useQuery(TEST_QUERY)
 
     console.log(data);
 
     return (
         <div>{
-            loading ? 'loading...' : data?.locations?.map(el => {
-                return (
-                    <p>{el?.name || ''}</p>
-                )
-            })
-        }</div>
+            loading ? 'loading...' : 'data fetched'}</div>
     )
 }
 
